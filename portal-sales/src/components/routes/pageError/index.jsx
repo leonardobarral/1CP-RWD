f5dfbd15928852533701e3881933b0cc563e39cb
leonardobarral <rm551472@fiap.com.br>
2023-09-05 18:33:35 -0300

    Criada página de error

---
+++ portal-sales/src/components/routes/pageError/index.jsx
@@ -0,0 +1,10 @@
+export default function Error(){
+    document.title = "Erro 404"
+    return(
+        <>
+            <main>
+                <h1>Error</h1>
+            </main>
+        </>
+    );
+}
