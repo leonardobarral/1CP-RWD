f5dff8c4806b44fe2f3a12b3ba84ba62a5155f44
leonardobarral <rm551472@fiap.com.br>
2023-09-05 18:30:56 -0300

    Criada página Home

---
+++ portal-sales/src/components/routes/pageHome/index.jsx
@@ -0,0 +1,10 @@
+export default function Home(){
+    document.title = "Home"
+    return(
+        <>
+            <main>
+                <h1>HOME</h1>
+            </main>
+        </>
+    );
+}
